bc99c52ea6c22c7e182a425adcb8a2075d674204
Pedro Del Gallego <pedro.delgallego@gmail.com>
2011-10-03 14:16:55 +0200

    Implemented deleting tickets feature

--- ticketee/ticketee/app/controllers/tickets_controller.rb
+++ ticketee/ticketee/app/controllers/tickets_controller.rb
@@ -33,6 +33,12 @@ class TicketsController < ApplicationController
   def edit
   end
 
+  def destroy
+    @ticket.destroy
+    flash[:notice] = "Ticket has been deleted."
+    redirect_to @project
+  end
+
   protected
   def find_project
     @project = Project.find(params[:project_id])
